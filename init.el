a0c2d48ecd9c53164743c1563fe519f70623de4e
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-12-13 16:25:00 +0700

    Turn default layout name Spacemacs into SPACEMACS

--- init.el
+++ init.el
@@ -197,7 +197,7 @@ values."
    ;; (default nil)
    dotspacemacs-ex-substitute-global t
    ;; Name of the default layout (default "Default")
-   dotspacemacs-default-layout-name "Spacemacs"
+   dotspacemacs-default-layout-name "SPACEMACS"
    ;; If non nil the default layout name is displayed in the mode-line.
    ;; (default nil)
    dotspacemacs-display-default-layout t
